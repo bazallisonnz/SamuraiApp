910d265f04b88d5fcfd2a77b539dd1b5b4ad37e4
baz <bazallison@hotmail.com>
2018-08-24 22:26:58 +1200

    Create solution. created initial database with migration, add relationships and another migration

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(sb => new {sb.SamuraiId, sb.BattleId});
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
